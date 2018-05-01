4e783a57f8f6e1ebab9aa183306730de1674a635
Lawson Caudill <lawson@caudillclan.us>
2018-05-01 13:07:39 -0300

    Added telemetry item interface and some initial framing.

---
+++ Naos.Telemetry.Domain/IAmTelemetryItem.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IAmTelemetryItem.cs" company="Naos">
+//    Copyright (c) Naos 2017. All Rights Reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Telemetry.Domain
+{
+    /// <summary>
+    /// Interface to define that an object is a telemetry item.
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1040:AvoidEmptyInterfaces", Justification = "Want an interface NOT an attribute.")]
+    public interface IAmTelemetryItem
+    {
+    }
+}
